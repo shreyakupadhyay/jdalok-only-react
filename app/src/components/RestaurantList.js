ad9bd85a17e16036ea6cb1bd66ab7ad87e343e22
shreyakupadhyay <shreyakupadhyay07@gmail.com>
2017-10-06 13:17:18 +0530

    added card, restaurantList

--- app/src/components/RestaurantList.js
+++ app/src/components/RestaurantList.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import './RestaurantList.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Button, Card, Image } from 'semantic-ui-react'
+
+const styles = {
+    container: {
+        margin: "auto",
+        width: "60%"
+    },
+    cards: {
+        width: "100%"
+    }
+}
 
 const urlCall = "http://localhost:8000/api/v1/restaurants/jhansi/index"
 
@@ -37,17 +49,34 @@ class RestaurantList extends Component {
 
         if(this.state.requestFailed) return <p>Failed!!!</p>        
         if(!this.state.restaurantData) return <p>Loading.....</p>
-        var restaurantList = this.state.restaurantData.restaurants;
+        var restaurants = this.state.restaurantData.restaurants;
         return (
-        <div className="App">
-            { restaurantList.map(item =>
-                <h2 key={item.id}> 
-                    {item.name}
-                </h2>
-            )
-
-            }
-        </div>
+            <div class="right-ct-container col-md-10 col-sm-10  padding0" style={ styles.container }>
+            { restaurants.map((item) => (
+                       <Card.Group>
+                        <Card style={ styles.cards }>
+                                <Card.Content>
+                                    <Image floated='right' size='mini' src='https://content.jdmagicbox.com/comp/jhansi/m5/9999px510.x510.160819185440.p5m5/catalogue/the-handi-restrurent-jhansi-xzojl.jpg' />
+                                    <Card.Header>
+                                    {item.name}
+                                    </Card.Header>
+                                    <Card.Meta>
+                                    {item.city}
+                                    </Card.Meta>
+                                    <Card.Description>
+                                    {item.address}
+                                    </Card.Description>
+                                </Card.Content>
+                                <Card.Content extra>
+                                    <div className='ui two buttons'>
+                                    <Button basic color='green'>Show Details</Button>
+                                    <Button basic color='red'>Contact</Button>
+                                    </div>
+                                </Card.Content>
+                            </Card>
+                        </Card.Group>
+                ) )}
+            </div>
         );
     }
 }
