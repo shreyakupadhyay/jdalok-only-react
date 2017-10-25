99f8ce7e4a94435abe725db50270cee91354e53b
shreyakupadhyay <shreyakupadhyay07@gmail.com>
2017-10-26 04:31:48 +0530

    updated Pagination CSS

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom'
 import DashBoard from './DashBoard'
 import Home from './Home/Home'
 import LoggedInHeader from './Header/LoggedInHeader/LoggedInHeader'
-import Pagination from './Footer/Pagination/PageDiv'
 
 
 const Main = () => (
@@ -12,7 +11,6 @@ const Main = () => (
             <Route exact path='/' component={DashBoard}/>
             {/* <Route path='/home' component={LoggedInHeader}/> */}
             <Route exact path='/home/restaurants' component={Home}/>    
-            <Route exact path='/pages' component={Pagination}/>    
                     
           
           {/* <Route exact path='/restaurant' component={RestaurantData}/> */}
